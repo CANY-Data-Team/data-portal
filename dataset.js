da49593b220b0af7566d5d08e63ed9ffbf0d3b64
cany_ccook <ccook@correctionalassociation.org>
2026-06-12 12:36:51 -0400

    debug details page

--- dataset.js
+++ dataset.js
@@ -44,3 +44,26 @@ function loadPreview(url) {
         `<pre>${rows.join("\n")}</pre>`;
     });
 }
+
+const id = new URLSearchParams(window.location.search).get("id");
+
+fetch("./data/datasets.json")
+  .then(r => r.json())
+  .then(data => {
+
+    const d = data.find(x => x.id === id);
+
+    if (!d) {
+      document.body.innerHTML = `
+        <h2>Dataset not found</h2>
+        <p>ID: ${id}</p>
+      `;
+      return;
+    }
+
+    document.getElementById("title").innerText = d.title;
+    document.getElementById("desc").innerText = d.description;
+  });
+
+  console.log("URL ID:", id);
+  console.log("DATASET FOUND:", d);
